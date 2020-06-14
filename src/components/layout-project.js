1712e813686849fb8c03eeb849691c4f8bfc5e80
Nico <n.santilio97@gmail.com>
2020-06-14 23:43:34 +0200

    aggiornato layout-project per dispositivi mobili

--- src/components/layout-project.js
+++ src/components/layout-project.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
-import layoutStyle from '../styles/layout.module.scss';
+import layoutStyle from '../styles/layout-project.module.scss';
 
 function LayoutProject(props) {
     return(
