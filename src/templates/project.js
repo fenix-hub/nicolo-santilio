c1d7046634f2c66e366b6355de92e252c6924d7d
Nico <n.santilio97@gmail.com>
2020-06-23 23:12:28 +0200

    new project layoud

--- src/templates/project.js
+++ src/templates/project.js
@@ -3,7 +3,7 @@ import LayoutProject from '../components/layout-project';
 import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Head from '../components/head';
-
+import { BLOCKS } from "@contentful/rich-text-types"
 
 export const query = graphql`
     query($slug: String!) {
@@ -13,6 +13,12 @@ export const query = graphql`
             publishedDate(formatString: "DD-MMMM-YYYY")
             body {
                 json
+            }
+            icon {
+                file {
+                    url
+                }
+                title
             }
         }
     }
@@ -24,14 +30,16 @@ export const options = {
             const alt = node.data.target.fields.title['en-US'];
             const src = node.data.target.fields.file['en-US'].url;
             return <img alt={alt} src={src}/>
-        }
-    }
+        },
+        [BLOCKS.HEADING_3]: (node, children) => <h3 id={children.toString().toLowerCase().split(" ").join("-")}>{children}</h3>,
+    },
 }
 
 function Project(props) {
     return (
         <LayoutProject>
             <Head title={props.data.contentfulProject.title}/>
+            <img src={props.data.contentfulProject.icon!=null ? props.data.contentfulProject.icon.file.url : ""} alt={props.data.contentfulProject.icon!=null ? props.data.contentfulProject.icon.title : ""} />
             <h1>{props.data.contentfulProject.title}</h1>
             <p>{props.data.contentfulProject.publishedDate}</p>
             <div>
