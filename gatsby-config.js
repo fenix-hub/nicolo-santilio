6831ad2b4bf85919efe0c7f8921d8a1c34fb851c
Nicolò Santilio <n.santilio97@gmail.com>
2021-05-16 03:40:14 +0200

    update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,28 +20,6 @@ module.exports = {
   pathPrefix: "/nicolo-santilio",
 
   plugins: [
-    {
-      resolve: 'gatsby-plugin-netlify',
-      options: {
-        headers: {
-          "/*": [
-            "Access-Control-Allow-Origin: *",
-            "Cross-Origin-Embedder-Policy: require-corp",
-            "Cross-Origin-Opener-Policy: same-origin"
-          ],
-        }, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [
-          "Access-Control-Allow-Origin: *",
-          "Cross-Origin-Embedder-Policy: require-corp",
-          "Cross-Origin-Opener-Policy: same-origin"
-        ], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-contentful',
